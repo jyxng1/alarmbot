fa6d00e8002d237fd1e69837c3bc7a6d31751753
Joey Xiang <joey.xiang426@gmail.com>
2024-01-05 21:34:05 -0800

    feat: get_recent_summary command first working version

--- alarmbot/src/ui/get_recent_summary.py
+++ alarmbot/src/ui/get_recent_summary.py
@@ -1,29 +1,71 @@
-import datetime
 import requests
-from .utils import CONSTANTS, get_item_from_dynamodb
+from datetime import datetime
+from .utils import CONSTANTS, get_user_info
 
 
-def retrieve_stats(data):
-    map = data.get('meta').get('map').get('name')
-    mode = data.get('meta').get('mode')
-    time = datetime.strptime(data.get('started_at'), "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%d/%m/%Y %H:%M:%S")
-    team = data.get('stats').get('team').lower()
-    agent = data.get('stats').get('character').get('name')
+def retrieve_stats(data, username, tag):
+    meta = data.get("meta")
+    stats = data.get("stats")
+
+    map = meta.get("map").get("name")
+    mode = meta.get("mode")
+    time = datetime.strptime(meta.get("started_at"), "%Y-%m-%dT%H:%M:%S.%fZ").strftime(
+        "%d/%m/%Y %H:%M:%S"
+    )
+
+    team = data.get("stats").get("team").lower()
+    enemy_team = "red" if team == "blue" else "blue"
+    rounds = data.get("teams")
+    team_rounds = rounds.get(team)
+    enemy_rounds = rounds.get(enemy_team)
+    total_rounds = team_rounds + enemy_rounds
+    result = (
+        "W"
+        if team_rounds > enemy_rounds
+        else "L"
+        if team_rounds < enemy_rounds
+        else "D"
+    )
+
+    agent = stats.get("character").get("name")
+    score = stats.get("score")
+    kills = stats.get("kills")
+    deaths = stats.get("deaths")
+    assists = stats.get("assists")
+    shots = stats.get("shots")
+    damage = stats.get("damage")
+
+    kda = f"{kills}/{deaths}/{assists}"
+    acs = str(round(score / total_rounds))
+    adr = str(round(damage.get("made") / total_rounds))
+    dmg_delta = str(round((damage.get("made") - damage.get("received")) / total_rounds))
+    kd = str(round(kills / deaths, 2))
+    hsr = "{:.0%}".format(
+        round(
+            shots.get("head")
+            / (shots.get("head") + shots.get("body") + shots.get("leg")),
+            2,
+        )
+    )
+
+    stats_message = f"""
+    Match summary for {username}#{tag}:
+    **{mode}** - __{time}__
+    {agent} ({map})
+    ({result}) {team_rounds}-{enemy_rounds}
+    ```
+    |    K/D/A    |   K/D   |   DDΔ   |   HSR   |   ADR   |   ACS   |
+    |{kda.center(13)}|{kd.center(9)}|{dmg_delta.center(9)}|{hsr.center(9)}|{adr.center(9)}|{acs.center(9)}|
+    ```
+    """
+
+    return stats_message
+
 
 def get_recent_summary(data, member):
-    options = data.get('options').get('options')
+    options = data.get("options")[0].get("options")
 
-    if options is None and options[0].get('name') is not "username" and options[1].get('name') is not "tag":
-        item = get_item_from_dynamodb(member.get('user').get('id'))
-        if item:
-            username = item.get('ValorantUsername')
-            tag = item.get('ValorantTag')
-        else:
-            message_content = "Please set a default account first or specify a username and tag"
-            return message_content
-    else:
-        username = options[0].get('value')
-        tag = data.get('options').get('options')[1].get('value')
+    username, tag = get_user_info(options, member)
 
     extras = "?size=1"
     for option in options:
@@ -32,12 +74,15 @@ def get_recent_summary(data, member):
         if option.get("name") == "mode":
             extras += "&mode=" + option.get("value")
 
-    response = requests.get(f"{CONSTANTS['API_URL']}/valorant/v1/lifetime/matches/na/{username}/{tag}{extras}")
+    response = requests.get(
+        f"{CONSTANTS['API_URL']}/valorant/v1/lifetime/matches/na/{username}/{tag}{extras}"
+    )
 
     if response.status_code == 200:
         data = response.json()
-        message_content = retrieve_stats(data.get('data')[0])
+        print(data)
+        message_content = retrieve_stats(data.get("data")[0], username, tag)
     else:
         message_content = f"Error from Valorant API: {response.status_code}"
-    
+
     return message_content
